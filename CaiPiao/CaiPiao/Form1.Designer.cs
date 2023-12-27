ea0d0584d6034b305c5047cc3b0611fc710298ff
FShH1997 <37678827+FShH1997@users.noreply.github.com>
2023-12-27 09:22:19 +0800

    Add files via upload

---
+++ CaiPiao/CaiPiao/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CaiPiao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_sign = new System.Windows.Forms.Label();
+            this.label_Rules = new System.Windows.Forms.Label();
+            this.label_nums = new System.Windows.Forms.Label();
+            this.radioButton_Rule_DLT = new System.Windows.Forms.RadioButton();
+            this.panel_Rules = new System.Windows.Forms.Panel();
+            this.radioButton_Rule0 = new System.Windows.Forms.RadioButton();
+            this.radioButton_Rule_SSQ = new System.Windows.Forms.RadioButton();
+            this.numericUpDown_nums = new System.Windows.Forms.NumericUpDown();
+            this.button_Debug = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.button_BIU = new System.Windows.Forms.Button();
+            this.checkBox_Clear = new System.Windows.Forms.CheckBox();
+            this.panel_Rules.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_nums)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_sign
+            // 
+            this.label_sign.AutoSize = true;
+            this.label_sign.Enabled = false;
+            this.label_sign.Location = new System.Drawing.Point(10, 10);
+            this.label_sign.Name = "label_sign";
+            this.label_sign.Size = new System.Drawing.Size(101, 12);
+            this.label_sign.TabIndex = 0;
+            this.label_sign.Text = "Designed by 大海";
+            // 
+            // label_Rules
+            // 
+            this.label_Rules.AutoSize = true;
+            this.label_Rules.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_Rules.Location = new System.Drawing.Point(30, 40);
+            this.label_Rules.Name = "label_Rules";
+            this.label_Rules.Size = new System.Drawing.Size(73, 21);
+            this.label_Rules.TabIndex = 1;
+            this.label_Rules.Text = "规则：";
+            // 
+            // label_nums
+            // 
+            this.label_nums.AutoSize = true;
+            this.label_nums.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_nums.Location = new System.Drawing.Point(30, 150);
+            this.label_nums.Name = "label_nums";
+            this.label_nums.Size = new System.Drawing.Size(73, 21);
+            this.label_nums.TabIndex = 3;
+            this.label_nums.Text = "数量：";
+            // 
+            // radioButton_Rule_DLT
+            // 
+            this.radioButton_Rule_DLT.AutoSize = true;
+            this.radioButton_Rule_DLT.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton_Rule_DLT.Location = new System.Drawing.Point(5, 9);
+            this.radioButton_Rule_DLT.Name = "radioButton_Rule_DLT";
+            this.radioButton_Rule_DLT.Size = new System.Drawing.Size(127, 24);
+            this.radioButton_Rule_DLT.TabIndex = 4;
+            this.radioButton_Rule_DLT.TabStop = true;
+            this.radioButton_Rule_DLT.Text = "体彩大乐透";
+            this.radioButton_Rule_DLT.UseVisualStyleBackColor = true;
+            // 
+            // panel_Rules
+            // 
+            this.panel_Rules.Controls.Add(this.radioButton_Rule0);
+            this.panel_Rules.Controls.Add(this.radioButton_Rule_SSQ);
+            this.panel_Rules.Controls.Add(this.radioButton_Rule_DLT);
+            this.panel_Rules.Location = new System.Drawing.Point(100, 30);
+            this.panel_Rules.Name = "panel_Rules";
+            this.panel_Rules.Size = new System.Drawing.Size(150, 90);
+            this.panel_Rules.TabIndex = 6;
+            // 
+            // radioButton_Rule0
+            // 
+            this.radioButton_Rule0.AutoSize = true;
+            this.radioButton_Rule0.Enabled = false;
+            this.radioButton_Rule0.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton_Rule0.Location = new System.Drawing.Point(5, 59);
+            this.radioButton_Rule0.Name = "radioButton_Rule0";
+            this.radioButton_Rule0.Size = new System.Drawing.Size(87, 24);
+            this.radioButton_Rule0.TabIndex = 6;
+            this.radioButton_Rule0.TabStop = true;
+            this.radioButton_Rule0.Text = "自定义";
+            this.radioButton_Rule0.UseVisualStyleBackColor = true;
+            this.radioButton_Rule0.Click += new System.EventHandler(this.radioButton_Rule0_Click);
+            // 
+            // radioButton_Rule_SSQ
+            // 
+            this.radioButton_Rule_SSQ.AutoSize = true;
+            this.radioButton_Rule_SSQ.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton_Rule_SSQ.Location = new System.Drawing.Point(5, 34);
+            this.radioButton_Rule_SSQ.Name = "radioButton_Rule_SSQ";
+            this.radioButton_Rule_SSQ.Size = new System.Drawing.Size(127, 24);
+            this.radioButton_Rule_SSQ.TabIndex = 5;
+            this.radioButton_Rule_SSQ.TabStop = true;
+            this.radioButton_Rule_SSQ.Text = "福彩双色球";
+            this.radioButton_Rule_SSQ.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown_nums
+            // 
+            this.numericUpDown_nums.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.numericUpDown_nums.Location = new System.Drawing.Point(100, 145);
+            this.numericUpDown_nums.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_nums.Name = "numericUpDown_nums";
+            this.numericUpDown_nums.Size = new System.Drawing.Size(150, 30);
+            this.numericUpDown_nums.TabIndex = 7;
+            this.numericUpDown_nums.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown_nums.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button_Debug
+            // 
+            this.button_Debug.Location = new System.Drawing.Point(117, 5);
+            this.button_Debug.Name = "button_Debug";
+            this.button_Debug.Size = new System.Drawing.Size(75, 23);
+            this.button_Debug.TabIndex = 8;
+            this.button_Debug.Text = "debug";
+            this.button_Debug.UseVisualStyleBackColor = true;
+            this.button_Debug.Visible = false;
+            this.button_Debug.Click += new System.EventHandler(this.button_Debug_Click);
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_Result.Location = new System.Drawing.Point(300, 30);
+            this.textBox_Result.Multiline = true;
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_Result.Size = new System.Drawing.Size(350, 300);
+            this.textBox_Result.TabIndex = 9;
+            // 
+            // button_BIU
+            // 
+            this.button_BIU.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_BIU.Location = new System.Drawing.Point(50, 200);
+            this.button_BIU.Name = "button_BIU";
+            this.button_BIU.Size = new System.Drawing.Size(200, 100);
+            this.button_BIU.TabIndex = 10;
+            this.button_BIU.Text = " 开奖！";
+            this.button_BIU.UseVisualStyleBackColor = true;
+            this.button_BIU.Click += new System.EventHandler(this.button_BIU_Click);
+            // 
+            // checkBox_Clear
+            // 
+            this.checkBox_Clear.AutoSize = true;
+            this.checkBox_Clear.Checked = true;
+            this.checkBox_Clear.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_Clear.Location = new System.Drawing.Point(154, 314);
+            this.checkBox_Clear.Name = "checkBox_Clear";
+            this.checkBox_Clear.Size = new System.Drawing.Size(96, 16);
+            this.checkBox_Clear.TabIndex = 11;
+            this.checkBox_Clear.Text = "覆盖右侧记录";
+            this.checkBox_Clear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.checkBox_Clear);
+            this.Controls.Add(this.button_BIU);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.button_Debug);
+            this.Controls.Add(this.numericUpDown_nums);
+            this.Controls.Add(this.panel_Rules);
+            this.Controls.Add(this.label_nums);
+            this.Controls.Add(this.label_Rules);
+            this.Controls.Add(this.label_sign);
+            this.Name = "Form1";
+            this.Text = "中了分我一半";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel_Rules.ResumeLayout(false);
+            this.panel_Rules.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_nums)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_sign;
+        private System.Windows.Forms.Label label_Rules;
+        private System.Windows.Forms.Label label_nums;
+        private System.Windows.Forms.RadioButton radioButton_Rule_DLT;
+        private System.Windows.Forms.Panel panel_Rules;
+        private System.Windows.Forms.RadioButton radioButton_Rule_SSQ;
+        private System.Windows.Forms.RadioButton radioButton_Rule0;
+        private System.Windows.Forms.NumericUpDown numericUpDown_nums;
+        private System.Windows.Forms.Button button_Debug;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.Button button_BIU;
+        private System.Windows.Forms.CheckBox checkBox_Clear;
+    }
+}
+
---
+++ CaiPiao/CaiPiao/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaiPiao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Rule rule_DLT_Red = new Rule(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35 }, 5);
+        Rule rule_DLT_Blue = new Rule(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, 2);
+        Rule rule_SSQ_Red = new Rule(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33 }, 6);
+        Rule rule_SSQ_Blue = new Rule(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }, 1);
+        
+
+        private void GetBalls(List<Rule> rules, int num)
+        {
+            for(int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < rules.Count; j++)
+                {
+                    if (j == 0)
+                    {
+                        ShowResult(GetBallsInRule(rules[j]));
+                    }
+                    else
+                    {
+                        ShowResult(" + ", GetBallsInRule(rules[j]));
+                    }
+                }
+                textBox_Result.AppendText(Environment.NewLine);
+            }
+        }
+        /// <summary>
+        /// 用指定的规则生成一组彩票号码组合
+        /// </summary>
+        /// <param name="rule">
+        /// Rule类的对象，表示“从几个有限的数字中选取若干个数字”的规则，例如表示双色球中红球的规则
+        /// </param>
+        /// <returns>
+        /// int[]类型的数组，表示选择出来的数字组合
+        /// </returns>
+        private int[] GetBallsInRule(Rule rule)
+        {
+            int[] indexOfBalls = new int[rule.GetAmount()]; //选中的数字在rule.nums数组中的下标
+            for(int i = 0;i < indexOfBalls.Length; i++)
+            {
+                indexOfBalls[i] = -1;
+            }
+            int indexOfBall_Now = 0;
+            double[] LuckOfBalls = new double[rule.GetNums().Length];
+            int seed = (int)DateTime.Now.Ticks;
+            Random random = new Random(seed);
+            do
+            {
+                double sumCheck = LuckOfBalls.Sum();
+                do
+                {
+                flag: int index_Luck = random.Next(LuckOfBalls.Length);
+                    foreach(int i in indexOfBalls)
+                    {
+                        if(i == index_Luck)
+                        {
+                            goto flag;
+                        }
+                    }
+                    if((LuckOfBalls[index_Luck] += (random.NextDouble() + 0.01)) >= 100)
+                    {
+                        indexOfBalls[indexOfBall_Now++] = index_Luck;
+                        break;
+                    }
+                    if (sumCheck >= (sumCheck = LuckOfBalls.Sum()))
+                    {
+                        MessageBox.Show("LuckOfBalls累加检查出错", "？");
+                        break;
+                    }
+                } while (true);
+            } while (indexOfBall_Now < indexOfBalls.Length);
+            Array.Sort(indexOfBalls);
+            int[] Results = new int[indexOfBalls.Length];
+            for(int i = 0; i < Results.Length; i++)
+            {
+                Results[i] = rule.GetNums()[indexOfBalls[i]];
+            }
+            return Results;
+        }
+        /// <summary>
+        /// 将数字组合在textBox_Result中追加显示
+        /// </summary>
+        /// <param name="result">
+        /// int[]类型的数组，表示选择出来的数字组合
+        /// </param>
+        private void ShowResult(int[] result)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i == 0)
+                {
+                    textBox_Result.AppendText(result[i].ToString());
+                }
+                else
+                {
+                    textBox_Result.AppendText(", " + result[i].ToString());
+                }
+            }
+        }
+        /// <summary>
+        /// 将数字组合在textBox_Result中追加显示，在数字组合开头添加指定字符串
+        /// </summary>
+        /// <param name="str_Head">
+        /// 在数字组合开头需要显示的字符串
+        /// </param>
+        /// <param name="result">
+        /// int[]类型的数组，表示选择出来的数字组合
+        /// </param>
+        private void ShowResult(string str_Head, int[] result)
+        {
+            textBox_Result.AppendText(str_Head);
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i == 0)
+                {
+                    textBox_Result.AppendText(result[i].ToString());
+                }
+                else
+                {
+                    textBox_Result.AppendText(", " + result[i].ToString());
+                }
+            }
+        }
+        /// <summary>
+        /// 将数字组合在textBox_Result中追加显示，在数字组合结尾添加指定字符串
+        /// </summary>
+        /// <param name="result">
+        /// int[]类型的数组，表示选择出来的数字组合
+        /// </param>
+        /// <param name="str_End">
+        /// 在数字组合结尾需要显示的字符串
+        /// </param>
+        private void ShowResult(int[] result, string str_End)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i == 0)
+                {
+                    textBox_Result.AppendText(result[i].ToString());
+                }
+                else
+                {
+                    textBox_Result.AppendText(", " + result[i].ToString());
+                }
+            }
+            textBox_Result.AppendText(str_End);
+        }
+        /// <summary>
+        /// 将数字组合在textBox_Result中追加显示，在数字组合开头和结尾添加指定字符串
+        /// </summary>
+        /// <param name="str_Head">
+        /// 在数字组合开头需要显示的字符串
+        /// </param>
+        /// <param name="result">
+        /// int[]类型的数组，表示选择出来的数字组合
+        /// </param>
+        /// <param name="str_End">
+        /// 在数字组合结尾需要显示的字符串
+        /// </param>
+        private void ShowResult(string str_Head, int[] result, string str_End)
+        {
+            textBox_Result.AppendText(str_Head);
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i == 0)
+                {
+                    textBox_Result.AppendText(result[i].ToString());
+                }
+                else
+                {
+                    textBox_Result.AppendText(", " + result[i].ToString());
+                }
+            }
+            textBox_Result.AppendText(str_End);
+        }
+        /// <summary>
+        /// debug
+        /// </summary>
+        private void button_Debug_Click(object sender, EventArgs e)
+        {
+            //int[] result = GetBallsInRule(rule_DLT_Red);
+            //foreach (int i in result)
+            //{
+            //    Console.WriteLine(i.ToString());
+            //}
+            //Console.WriteLine("\n");
+            //ShowResult(result);
+            //result = GetBallsInRule(rule_DLT_Blue);
+            //foreach (int i in result)
+            //{
+            //    Console.WriteLine(i.ToString());
+            //}
+            //Console.WriteLine("\n");
+            //ShowResult(result);
+
+        }
+        /// <summary>
+        /// 不重要
+        /// </summary>
+        private void radioButton_Rule0_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("敬请期待", "~");
+        }
+        /// <summary>
+        /// 使textBox_Result的大小随窗口大小变化而变化
+        /// </summary>
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            textBox_Result.Size = new Size(this.Size.Width - 350, this.Size.Height - 100);
+        }
+        /// <summary>
+        /// 按下按钮后的执行函数，实现随机生成指定数量的对应规则的数字组合，并显示在textBox_Result中
+        /// </summary>
+        private void button_BIU_Click(object sender, EventArgs e)
+        {
+            if(checkBox_Clear.Checked)
+            {
+                textBox_Result.Clear();
+            }
+            if(radioButton_Rule_DLT.Checked)
+            {
+                List<Rule> rules_DLT = new List<Rule>();
+                rules_DLT.Add(rule_DLT_Red);
+                rules_DLT.Add(rule_DLT_Blue);
+                GetBalls(rules_DLT, (int)numericUpDown_nums.Value);
+            }
+            else if (radioButton_Rule_SSQ.Checked)
+            {
+                List<Rule> rules_SSQ = new List<Rule>();
+                rules_SSQ.Add(rule_SSQ_Red);
+                rules_SSQ.Add(rule_SSQ_Blue);
+                GetBalls(rules_SSQ, (int)numericUpDown_nums.Value);
+            }
+            else
+            {
+                MessageBox.Show("你咋点出来的？？？", "？？？");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    /// <summary>
+    /// Rule类表示彩票规则，建议以List<Rule>形式使用
+    /// 成员int[] nums表示数字范围
+    /// 成员int amount表示范围内需要选取的数字数量
+    /// 例如nums = {1, 2, 3, 4, 5}; amount = 2时，表示从1至5中选取2个数
+    /// 以List<Rule>的形式可表示例如大乐透“1-35内选5个数，1-12内选2个数”这类规则
+    /// </summary>
+    /// 
+    internal partial class Rule
+    {
+        private int[] nums = new int[1];
+        private int amount = 1;
+
+        public Rule(int[] newNums, int newAmount)
+        {
+            this.nums = newNums;
+            this.amount = newAmount;
+        }
+
+        public int[] GetNums()
+        {
+            return this.nums;
+        }
+
+        public int GetAmount()
+        {
+            return this.amount;
+        }
+    }
+}
